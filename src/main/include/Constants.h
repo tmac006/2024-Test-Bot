e9da981aa3398b06d3be49cbfaed1592da415065
trent <trentmac06@gmail.com>
2024-01-22 11:15:29 -0500

    Started Amp, Deleted example stuff, general cleanup

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -4,6 +4,10 @@
 //CAN VALUES
 #define PCM_CAN 0
 
+//Limit Switches
+#define AP_TOP_LIMIT_SWITCH 3
+#define AP_BOT_LIMIT_SWITCH 4
+
 //PCM PLACEHOLDER VALUES 
 #define INTAKE_DEPLOY_PCM 1
 #define INTAKE_RETRACT_PCM 2
--- src/main/include/subsystems/AMPeratius.h
+++ src/main/include/subsystems/AMPeratius.h
@@ -1,21 +1,40 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include <frc/DigitalInput.h>
+#include "Constants.h"
 
 class AMPeratius : public frc2::SubsystemBase {
- public:
+ public: //AMPeratius Shortened To AP cause I aint writing all that
   AMPeratius();
+  
+  void   AMPeratiusInit(void);
+  //Meaty Stuff
+  void   SetAPPower(double power);
+  void   SetAPAngle(double angle);
+  double GetAPPower(void);
+  double GetAPAngle(void);
+  double GetCurrentAPAngle(void);
+  //Limit Switches
+  bool   GetTopAPLimitSW(void);
+  bool   GetBotAPLimitSW(void);
+  //Encoders
+  void   ResetAPEncoder(void);
+  int    GetAPEncoder(void);
 
-  /**
-   * Will be called periodically whenever the CommandScheduler runs.
-   */
+  
   void Periodic() override;
 
  private:
-  // Components (e.g. motor controllers and sensors) should generally be
-  // declared private and exposed only through public methods.
+    frc::DigitalInput m_topAPLimitSwitch    {AP_TOP_LIMIT_SWITCH};
+    frc::DigitalInput m_botAPLimtSwitch     {AP_BOT_LIMIT_SWITCH};
+
+
+    double m_APAngle;
+    double m_APPower;
+    double m_wantedAPAngle;
+    
+
+  
+  
 };
